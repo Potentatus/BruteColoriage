3c66953734b8721a7b491d703247266015414b16
Potus <krzyczkowskiw@student.mini.pw.edu.pl>
2019-05-22 16:42:48 +0200

    not working....why?

--- BRUTE_FORCE/kernel.cu
+++ BRUTE_FORCE/kernel.cu
@@ -8,6 +8,7 @@
 #include <cmath>
 #include <climits>
 #include <time.h>
+#include <new>
 
 __constant__ int EdgeTable[50];
 __constant__ int VertexTable[50];
@@ -18,9 +19,27 @@ struct ResultData
 	int color_index;
 };
 
-void Initialize_graph(int * e_t, int * v_t, int * n, int * e)
+void Initialize_graph(int*& e_t, int *& v_t, int & n, int & e)
 {
-	//TODO read data
+	e = 8;
+	e_t = new int[e];
+	e_t[0] = 5;
+	e_t[1] = 5;
+	e_t[2] = 4;
+	e_t[3] = 3;
+	e_t[4] = 5;
+	e_t[5] = 1;
+	e_t[6] = 2;
+	e_t[7] = 4;
+
+	n = 5;
+	v_t = new int[n];
+	v_t[0] = 0;
+	v_t[1] = 1;
+	v_t[2] = 2;
+	v_t[3] = 3;
+	v_t[4] = 5;
+	v_t[5] = 8;
 }
 
 cudaError_t ColorWithCuda(int *c, int *e, int *size_c, int *size_e, bool* flag, int * result);
@@ -29,7 +48,7 @@ __device__ bool TestColoriage(int * coloriage, int size_v)
 {
 	bool result = true;
 	int current_vertex = 0;
-	while (result && current_vertex != size_v)
+	while (result && current_vertex < size_v)
 	{
 		//get edges range
 		int start_edge_index = VertexTable[current_vertex];
@@ -47,6 +66,15 @@ __device__ bool TestColoriage(int * coloriage, int size_v)
 	}
 	return result;
 }
+__device__ int dev_pow(int a, int b)
+{
+	uint64_t result = 1;
+	while (b-- > 0)
+	{
+		result *= a;
+	}
+	return result;
+}
 
 __global__ void BruteForceKernel(const int *n, int *output, bool * found_flag)
 {
@@ -57,12 +85,17 @@ __global__ void BruteForceKernel(const int *n, int *output, bool * found_flag)
 	//numer w¹tku
 	uint64_t tid = blockId_grid * threads_per_block + threadIdx.x + threadIdx.y*blockDim.x + threadIdx.z*blockDim.x*blockDim.y;
 	//liczba kolorowañ
-	uint64_t all_results = pow(3, *n);
+	uint64_t all_results = dev_pow(3, *n);
 	//dzielimy przedzia³ kolorowañ (funkcja numeru w¹tku i iloœci w¹tków) - ka¿dy pe³ny, ostatni resztki z dzielenia
 	uint64_t threads = blockDim.x*blockDim.y*blockDim.z*gridDim.x*gridDim.y*gridDim.z;
 	uint64_t colors_count = (uint64_t)(all_results / threads + 1);
 	uint64_t start_key = tid * colors_count;
 	uint64_t end_key = start_key + colors_count;
+
+	if (start_key >= all_results)
+		return;
+	if (start_key + colors_count >= all_results)
+		colors_count = all_results - start_key;
 
 	while (!(*found_flag) && start_key < end_key)
 	{
@@ -83,26 +116,34 @@ __global__ void BruteForceKernel(const int *n, int *output, bool * found_flag)
 		//kolejny klucz
 		start_key += 1;
 	}
+	if (tid == 0)
+		for (int i = 0; i < *n; i++)
+			colors[i] = i;
 }
 
 int main()
 {
 	int *edge_table, *vertex_table;
-	int *n, *e;
+	int n, e;
 	int *output;
 	bool flag = false;
 
 	//read and translate graph
 	Initialize_graph(edge_table, vertex_table, n, e);
 
+	output = new int[n];
+
     // Add vectors in parallel.
-    cudaError_t cudaStatus = ColorWithCuda(edge_table, vertex_table, n, e, &flag, output);
+    cudaError_t cudaStatus = ColorWithCuda(edge_table, vertex_table, &n, &e, &flag, output);
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
+        fprintf(stderr, "ColorWithCuda failed! %d", cudaStatus);
         return 1;
     }
 
-	printf("Done. Found = %b",flag);
+	printf("Done. Found = %s\n", flag?"true":"false");
+
+	for (int i = 0; i < n; i++)
+		printf("%d -> %d\n", i, output[i]);
 
     // cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
@@ -111,18 +152,19 @@ int main()
         fprintf(stderr, "cudaDeviceReset failed!");
         return 1;
     }
+	delete[] edge_table;
+	delete[] vertex_table;
+	delete[] output;
 
     return 0;
 }
 
 // Helper function for using CUDA to add vectors in parallel.
-cudaError_t ColorWithCuda(int *e_t, int *v_t, int *n, int *e, bool * flag, int * output)
+cudaError_t ColorWithCuda(int e_t[], int v_t[], int *n, int *e, bool * flag, int* output)
 {
-    int *dev_e_t = 0;
-    int *dev_v_t = 0;
-    int *dev_n = 0;
-	int *dev_output = 0;
-	bool *dev_flag = 0;
+    int *dev_e_t, *dev_v_t, *dev_n;
+	int *dev_output;
+	bool *dev_flag;
     cudaError_t cudaStatus;
 
     // Choose which GPU to run on, change this on a multi-GPU system.
@@ -189,7 +231,7 @@ cudaError_t ColorWithCuda(int *e_t, int *v_t, int *n, int *e, bool * flag, int *
 	}
 
     // Launch a kernel on the GPU with one thread for each element.
-	BruteForceKernel <<<1, 512>>>(dev_n, dev_output);
+	BruteForceKernel <<<1, 512>>>(dev_n, dev_output, dev_flag);
 
     // Check for any errors launching the kernel
     cudaStatus = cudaGetLastError();
